73d30f8ec9d02a4eebf8cbf834999b0d47b08cb2
PAM <anphamdeveloper@gmail.com>
2022-06-29 18:48:18 +0700

    fix: fix dynamic layout menu

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Observers\UserObserver;
 use App\PAM\ApiResponse;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -29,6 +30,10 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->defineGate();
         $this->defineObserver();
+
+        Route::matched(function () {
+            $this->defineLayoutMenu();
+        });
     }
 
     protected function defineGate()
@@ -43,5 +48,113 @@ class AppServiceProvider extends ServiceProvider
     protected function defineObserver()
     {
         User::observe(UserObserver::class);
+    }
+
+    protected function defineLayoutMenu()
+    {
+        config(['web-config.layouts.menu' => [
+            'main' => [
+                [
+                    'label' => 'Home',
+                    'class' => '',
+                    'icon' => "<i data-feather='activity'></i>",
+                    'target' => url('/')
+                ],
+                [
+                    'label' => 'Account',
+                    'class' => '',
+                    'icon' => "<i data-feather='info'></i>",
+                    'target' => 'javascript:;',
+                    'auth' => true,
+                    'items' => [
+                        [
+                            'label' => 'Profile',
+                            'class' => '',
+                            'icon' => '<i class="fa-regular fa-user"></i>',
+                            'target' => route('account.profile')
+                        ],
+                        [
+                            'label' => 'Reset Password',
+                            'class' => '',
+                            'icon' => "<i data-feather='key'></i>",
+                            'target' => route('account.reset-password')
+                        ],
+                        [
+                            'label' => 'API',
+                            'class' => '',
+                            'icon' => "<i data-feather='tool'></i>",
+                            'target' => route('account.api')
+                        ],
+                    ]
+                ],
+            ],
+            'account' => [
+                [
+                    'label' => 'Profile',
+                    'class' => '',
+                    'target' => route('account.profile')
+                ],
+                [
+                    'label' => 'Reset Password',
+                    'class' => '',
+                    'target' => route('account.reset-password')
+                ],
+                [
+                    'label' => 'API',
+                    'class' => '',
+                    'target' => route('account.api')
+                ],
+            ],
+            'admin' => [
+                [
+                    'group' => 'Admin Area',
+                    'items' => [
+                        [
+                            'label' => 'Services',
+                            'class' => '',
+                            'icon' => "<i data-feather='server'></i>",
+                            'target' => route('admin.service.index'),
+                            'extraMatched' => 'admin\/services(.+)'
+                        ],
+                        [
+                            'label' => 'Users',
+                            'class' => '',
+                            'icon' => "<i data-feather='users'></i>",
+                            'target' => route('admin.user.index'),
+                            'extraMatched' => 'admin\/users(.+)'
+                        ],
+                        [
+                            'label' => 'Blacklisted',
+                            'class' => '',
+                            'icon' => "<i data-feather='hexagon'></i>",
+                            'target' => 'javascript:;',
+                            'items' => [
+                                [
+                                    'label' => 'Users',
+                                    'class' => '',
+                                    'icon' => "<i data-feather='users'></i>",
+                                    'target' => route('admin.blacklisted.user.index'),
+                                    'extraMatched' => 'admin\/user-blacklisted(.+)'
+                                ],
+                            ]
+                        ],
+                        [
+                            'label' => 'Banks',
+                            'class' => '',
+                            'icon' => "<i data-feather='dollar-sign'></i>",
+                            'target' => route('admin.bank.index'),
+                            'extraMatched' => 'admin\/banks(.+)'
+                        ],
+                        [
+                            'label' => 'Recharge History',
+                            'class' => '',
+                            'icon' => "<i data-feather='dollar-sign'></i>",
+                            'target' => route('admin.recharge-history.index'),
+                            'extraMatched' => 'admin\/recharge-histories(.+)'
+                        ],
+                    ]
+                ],
+            ],
+        ]]);
     }
 }
