2eedb11b648196cddce9bce313cde2137e0e3484
PAM <anphamdeveloper@gmail.com>
2022-07-14 22:42:43 +0700

    fix: fix auto acb bank

---
+++ database/migrations/2022_07_14_224106_add_extras_to_recharge_histories_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::whenTableDoesntHaveColumn('recharge_histories', 'extras', function (Blueprint $table) {
+            $table->json('extras')->nullable()->after('note');
+        });
+    }
+
+    public function down()
+    {
+        Schema::whenTableHasColumn('recharge_histories', 'extras', function (Blueprint $table) {
+            $table->dropColumn('extras');
+        });
+    }
+};
