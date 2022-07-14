1144d604943728d653caab16386e7d9e595ca3a0
PAM <anphamdeveloper@gmail.com>
2022-07-14 20:30:41 +0700

    feat: update

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -6,7 +6,6 @@ use App\Models\Order;
 use App\Models\Service;
 use App\PAM\Enums\ProductStatus;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
 
 class OrderController extends Controller
 {
@@ -14,44 +13,37 @@ class OrderController extends Controller
 
     public function __invoke(Request $request)
     {
-        $cacheTag = sprintf(self::$cacheKey, auth()->id());
-        $cacheKey = md5(json_encode($request->toArray()));
+        $search = $request->get('search');
+        $params = $request->except('search');
 
+        $records = Order::query()
+            ->with(['product' => ['order', 'service']])
+            ->whereUserId(auth()->id())
+            ->orderByDesc('id');
         $services = Service::whereVisible(true)->get(['id', 'name'])->pluck('name', 'id');
-        $paginationData = Cache::tags($cacheTag)->rememberForever($cacheKey, function () use ($request) {
-            $search = $request->get('search');
-            $params = $request->except('search');
 
-            $records = Order::query()
-                ->with(['product' => ['order', 'service']])
-                ->whereUserId(auth()->id())
-                ->orderByDesc('id');
+        search_relation_by_cols($records, $search, [
+            'product' => [
+                'id',
+                'mail',
+                'password',
+                'recovery_mail'
+            ]
+        ]);
 
-            search_relation_by_cols($records, $search, [
-                'product' => [
-                    'id',
-                    'mail',
-                    'password',
-                    'recovery_mail'
-                ]
-            ]);
-
-            query_by_cols($records, ['id'], $params);
-            query_relation_by_cols($records, [
-                'product' => [
-                    'status',
-                    'service_id'
-                ]
-            ], $params);
-
-            return paginate_with_params($records, $params);
-        });
+        query_by_cols($records, ['id'], $params);
+        query_relation_by_cols($records, [
+            'product' => [
+                'status',
+                'service_id'
+            ]
+        ], $params);
 
         return inertia('Orders/Manager', [
             'statusHtmlLabel' => ProductStatus::toBadgeHtmlArray(),
             'statusLabel' => ProductStatus::toLabelArray(),
             'services' => $services,
-            'paginationData' => $paginationData
+            'paginationData' => paginate_with_params($records, $params)
         ]);
     }
 }
--- app/Observers/OrderObserver.php
+++ app/Observers/OrderObserver.php
@@ -8,27 +8,19 @@ use Illuminate\Support\Facades\Cache;
 
 class OrderObserver
 {
-    public function cacheTag(Order $order)
-    {
-        return sprintf(OrderController::$cacheKey, $order->user_id);
-    }
-
     public function created(Order $order)
     {
         ServiceObserver::sendSocket($order->product->service_id);
-        Cache::tags($this->cacheTag($order))->flush();
     }
 
     public function updated(Order $order)
     {
         ServiceObserver::sendSocket($order->service_id);
-        Cache::tags($this->cacheTag($order))->flush();
     }
 
     public function deleted(Order $order)
     {
         ServiceObserver::sendSocket($order->service_id);
-        Cache::tags($this->cacheTag($order))->flush();
     }
 
 }
