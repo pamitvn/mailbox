49259d6894baeca31ad49c14b237e8d1395027b9
PAM <anphamdeveloper@gmail.com>
2022-07-26 22:16:39 +0700

    feat: update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,14 +10,14 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('clean:products')->hourly();
-        $schedule->command('clean:product-after-one-day')->hourly();
+//        $schedule->command('clean:product-after-one-day')->hourly();
         $schedule->command('recharge:acb')->everyMinute();
     }
 
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
