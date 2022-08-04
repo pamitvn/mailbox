c061aa35fffb87be33b5c648f5838c9194443c00
PAM <anphamdeveloper@gmail.com>
2022-08-04 15:03:36 +0700

    feat: update

--- database/migrations/2022_08_04_145800_change_payload_from_products_table.php
+++ database/migrations/2022_08_04_145800_change_payload_from_products_table.php
@@ -9,10 +9,12 @@ return new class extends Migration
     public function up()
     {
         Schema::whenTableHasColumn('products', 'payload', function (Blueprint $table) {
+            $table->dropColumn('payload');
+        });
+        Schema::whenTableDoesntHaveColumn('products', 'payload', function (Blueprint $table) {
             $table->string('payload', 768)
                 ->unique()
-                ->nullable(false)
-                ->change();
+                ->nullable(false);
         });
     }
 
