2eef35848f21872bae0fc8ec6e4eeeee5f3336b4
PAM <anphamdeveloper@gmail.com>
2022-08-08 19:06:15 +0700

    feat: update

--- app/Console/Commands/ProductCheckLiveFacebookCommand.php
+++ app/Console/Commands/ProductCheckLiveFacebookCommand.php
@@ -68,7 +68,7 @@ class ProductCheckLiveFacebookCommand extends Command
                 ->filter(fn ($ite) => filled($ite));
 
             $uuid = $data->pluck('uid');
-
+            $this->info($uuid);
             $response = $this->client->post($checkEndpoint, [
                 'json' => [
                     'uuid' => $uuid->toArray(),
