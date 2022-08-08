514da271c29ae84d0a3150eaae75d0fc43720f99
PAM <anphamdeveloper@gmail.com>
2022-08-08 13:08:45 +0700

    feat: update VSimpleStatCard.vue

--- resources/app/components/VSimpleStatCard.vue
+++ resources/app/components/VSimpleStatCard.vue
@@ -7,7 +7,7 @@
          <p class='ml-16 text-sm font-medium text-gray-500 truncate'>{{ label }}</p>
       </dt>
       <dd class='ml-16 pb-6 flex items-baseline sm:pb-7'>
-         <p class='oldstyle-nums text-2xl font-semibold text-gray-900'>
+         <p class='text-2xl font-semibold text-gray-900'>
             {{ stat }}
          </p>
       </dd>
